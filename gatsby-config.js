70ae9aaa6b829a51b984a0a7996e7718e9c9673a
Bruno Nogueira <bruno@brunon.com.br>
2020-05-25 23:17:04 -0300

    Incluindo styled-components para estilos no blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     author: `Bruno Nogueira`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
