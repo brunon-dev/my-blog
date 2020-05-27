00f576f5a1a83ae4190e10d3666a858fd13c3a6c
Bruno Nogueira <bruno@brunon.com.br>
2020-05-26 23:52:42 -0300

    Inclui estilo e implementa componentes da Sidebar

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,28 @@
+const links = [
+    {
+        label: 'Youtube',
+        url: 'https://www.youtube.com/canalbrunon'
+    },
+    {
+        label: 'Instagram',
+        url: 'https://www.instagram.com/eu_brunon/'
+    },
+    {
+        label: 'Facebook',
+        url: 'https://www.facebook.com/site.brunon/'
+    },
+    {
+        label: 'Twitter',
+        url: 'https://twitter.com/eu_brunon'
+    },
+    {
+        label: 'Linkedin',
+        url: 'https://www.linkedin.com/in/brunofgrd/'
+    },
+    {
+        label: 'Github',
+        url: 'https://github.com/eu-brunon'
+    }
+]
+
+export default links
---
+++ src/components/SocialLinks/icons.js
@@ -0,0 +1,17 @@
+import { Youtube } from '@styled-icons/icomoon/Youtube'
+import { Instagram } from '@styled-icons/icomoon/Instagram'
+import { Facebook } from '@styled-icons/icomoon/Facebook'
+import { Twitter } from '@styled-icons/icomoon/Twitter'
+import { Linkedin } from '@styled-icons/icomoon/Linkedin'
+import { Github } from '@styled-icons/icomoon/Github'
+
+const Icons = {
+    Youtube,
+    Instagram,
+    Facebook,
+    Twitter,
+    Linkedin,
+    Github
+}
+
+export default Icons
