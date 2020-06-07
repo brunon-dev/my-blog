b2a44b6a04398011c0705f2f728ffbe55de94f33
Bruno Nogueira <bruno@brunon.com.br>
2020-06-06 23:26:17 -0300

    Configura navegação entre posts e cometários com Disqus

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,11 +3,15 @@ import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
+import RecommendedPosts from '../components/RecommendedPosts'
+import Comments from '../components/Comments'
 
 import * as S from '../components/Post/styled'
 
-const BlogPost = ({ data }) => {
+const BlogPost = ({ data, pageContext }) => {
     const post = data.markdownRemark
+    const next = pageContext.nextPost
+    const previous = pageContext.previousPost
 
     return (
         <Layout>
@@ -18,10 +22,12 @@ const BlogPost = ({ data }) => {
               </S.PostDate>
               <S.PostTitle>{post.frontmatter.title}</S.PostTitle>
               <S.PostDescription>{post.frontmatter.description}</S.PostDescription>
-              <S.MainContent>
-                <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
-              </S.MainContent>
             </S.PostHeader>
+            <S.MainContent>
+              <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+            </S.MainContent>
+            <RecommendedPosts next={next} previous={previous} />
+            <Comments url={post.fields.slug} title={post.frontmatter.title} />
         </Layout>
     )
 }
@@ -29,6 +35,9 @@ const BlogPost = ({ data }) => {
 export const query = graphql`
   query Post($slug: String!) {
     markdownRemark(fields: {slug: {eq: $slug}}) {
+      fields {
+        slug
+      }
       frontmatter {
         title
         description
