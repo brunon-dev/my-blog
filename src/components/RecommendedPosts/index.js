6043a1346c4632bcb1bf2ac711c3ba33d185b157
Bruno Nogueira <bruno@brunon.com.br>
2020-06-13 23:50:45 -0300

    Inclui transição entre as páginas

--- src/components/RecommendedPosts/index.js
+++ src/components/RecommendedPosts/index.js
@@ -5,10 +5,26 @@ import * as S from './styled'
 const RecommendedPosts = ({ next, previous }) => (
     <S.RecommendedWrapper>
         {previous && (
-            <S.RecommendedLink to={previous.fields.slug} className="previous">{previous.frontmatter.title}</S.RecommendedLink>
+            <S.RecommendedLink
+                to={previous.fields.slug}
+                cover
+                direction="left"
+                bg="#16202c"
+                duration={0.6}
+                className="previous">
+                {previous.frontmatter.title}
+            </S.RecommendedLink>
         )}
         {next && (
-            <S.RecommendedLink to={next.fields.slug} className="next">{next.frontmatter.title}</S.RecommendedLink>
+            <S.RecommendedLink
+                to={next.fields.slug}
+                cover
+                direction="right"
+                bg="#16202c"
+                duration={0.6}
+                className="next">
+                {next.frontmatter.title}
+            </S.RecommendedLink>
         )}
     </S.RecommendedWrapper>
 )
